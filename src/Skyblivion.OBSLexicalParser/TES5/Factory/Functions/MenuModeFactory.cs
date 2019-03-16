295b48bd4909c78001cb9359ef9843f556a6b756
wessleym <wessleym@hotmail.com>
2019-03-16 17:03:20 -0500

    Fixing MenuModeFactory's case

---
+++ src/Skyblivion.OBSLexicalParser/TES5/Factory/Functions/MenuModeFactory.cs
@@ -0,0 +1,23 @@
+using Skyblivion.OBSLexicalParser.TES4.AST.Value.FunctionCall;
+using Skyblivion.OBSLexicalParser.TES5.AST;
+using Skyblivion.OBSLexicalParser.TES5.AST.Code;
+using Skyblivion.OBSLexicalParser.TES5.AST.Object;
+using Skyblivion.OBSLexicalParser.TES5.AST.Scope;
+
+namespace Skyblivion.OBSLexicalParser.TES5.Factory.Functions
+{
+    class MenuModeFactory : IFunctionFactory
+    {
+        private TES5ObjectCallFactory objectCallFactory;
+        public MenuModeFactory(TES5ObjectCallFactory objectCallFactory)
+        {
+            this.objectCallFactory = objectCallFactory;
+        }
+
+        public ITES5ValueCodeChunk ConvertFunction(ITES5Referencer calledOn, TES4Function function, TES5CodeScope codeScope, TES5GlobalScope globalScope, TES5MultipleScriptsScope multipleScriptsScope)
+        {
+            //return new TES5Integer(0);//WTM:  Change
+            return objectCallFactory.CreateObjectCall(TES5StaticReference.Utility, "IsInMenuMode", multipleScriptsScope);
+        }
+    }
+}
